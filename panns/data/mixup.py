8baffc5014f220b45a1866beb475d5450b596f0d
Alexander Bakhtin <alexander.bakhtin@tuni.fi>
2022-08-20 17:58:19 +0300

    add: docstrings

--- panns/data/mixup.py
+++ panns/data/mixup.py
@@ -5,17 +5,17 @@ __all__ = ['Mixup',
 
 class Mixup(object):
     def __init__(self, mixup_alpha, random_seed=1234):
-        """Mixup coefficient generator.
-        """
+        """Mixup coefficient generator."""
+
         self.mixup_alpha = mixup_alpha
         self.random_state = np.random.RandomState(random_seed)
 
     def get_lambda(self, batch_size):
-        """Get mixup random coefficients.
-        Args:
-          batch_size: int
-        Returns:
-          mixup_lambdas: (batch_size,)
+        """Mixup random coefficients.
+
+           :param int batch_size: batch size of the dataset
+           :return mixup_lambdas: Array of lambda coefficients, with shape (batch_size,)
+           :rtype: numpy.ndarray
         """
         mixup_lambdas = []
         for n in range(0, batch_size, 2):
@@ -26,16 +26,16 @@ class Mixup(object):
         return np.array(mixup_lambdas)
 
 def do_mixup(x, mixup_lambda):
-    """Mixup x of even indexes (0, 2, 4, ...) with x of odd indexes
-    (1, 3, 5, ...).
+    """Mixup x of even indexes (0, 2, 4, ...) with x of odd indexes (1, 3, 5, ...).
 
-    Args:
-      x: (batch_size * 2, ...)
-      mixup_lambda: (batch_size * 2,)
-
-    Returns:
-      out: (batch_size, ...)
+      :param x: array of shape (batch_size * 2, ...) to be mixed
+      :type x: numpy.ndarray
+      :param mixup_lambda: array of shape (batch_size * 2,), coefficients for mixup
+      :type mixup_lambda: numpy.ndarray 
+      :return out: Array of shape (batch_size, ...) with performed mixup
+      :rtype: numpy.ndarray
     """
+
     out = (x[0 :: 2].transpose(0, -1) * mixup_lambda[0 :: 2] + \
         x[1 :: 2].transpose(0, -1) * mixup_lambda[1 :: 2]).transpose(0, -1)
     return out
