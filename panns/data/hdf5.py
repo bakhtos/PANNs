528a734c8c8cf13c577b49d3735378fee5c7812c
Alexander Bakhtin <alexander.bakhtin@tuni.fi>
2022-08-23 13:41:51 +0300

    add: logging to all hdf5 functions, argument for logs' directory

--- panns/data/hdf5.py
+++ panns/data/hdf5.py
@@ -15,7 +15,7 @@ from panns.utils.logging_utils import create_logging
 from panns.utils.array_utils import float32_to_int16, pad_or_truncate
 
 def wav_to_hdf5(*, audios_dir, csv_path, hdf5_path,
-                   class_list_path, class_codes_path,
+                   class_list_path, class_codes_path, logs_dir=None,
                    clip_length=10000, sample_rate=32000, classes_num=110,
                    mini_data=0):
     """Pack waveform and target of several audio clips to a single hdf5 file. 
@@ -25,6 +25,8 @@ def wav_to_hdf5(*, audios_dir, csv_path, hdf5_path,
        :param str hdf5_path: Path to save the hdf5-packed file
        :param str class_list_path: Path to txt file with list of select classes'
                                    identifiers, one on each line
+       :param str logs_dir: Directory to save logs into, if None creates a
+                            directory 'logs' in CWD (default None)
        :param str class_codes_file: Path to tsv file that matches class codes to labels
        :param int clip_length: Length (in ms) of audio clips used in the dataset (default 10000)
        :param int sample_rate: Sample rate of packed audios (default 32000)
@@ -41,8 +43,8 @@ def wav_to_hdf5(*, audios_dir, csv_path, hdf5_path,
 
     create_folder(os.path.dirname(hdf5_path))
 
-    logs_dir = os.path.join('_logs', 'pack_waveforms_to_hdf5',get_filename(csv_path))
-    create_folder(logs_dir)
+    if logs_dir is None:
+        logs_dir = os.path.join(os.getcwd(), 'logs')
     create_logging(logs_dir, filemode='w')
     logging.info('Write logs to {}'.format(logs_dir))
     
@@ -56,7 +58,7 @@ def wav_to_hdf5(*, audios_dir, csv_path, hdf5_path,
     audios_num = len(audio_names)
 
     # Pack waveform to hdf5
-    total_time = time.time()
+    start_time = time.time()
 
     with h5py.File(hdf5_path, 'w') as hf:
         hf.create_dataset('audio_name', shape=((audios_num,)), dtype='S20')
@@ -69,7 +71,7 @@ def wav_to_hdf5(*, audios_dir, csv_path, hdf5_path,
             audio_path = os.path.join(audios_dir, audio_names[n])
 
             if os.path.isfile(audio_path):
-                logging.info('{} {}'.format(n, audio_path))
+                logging.info(f'{n} - {audio_path}')
                 (audio, _) = librosa.core.load(audio_path, sr=sample_rate, mono=True)
                 audio = pad_or_truncate(audio, clip_samples)
 
@@ -77,23 +79,32 @@ def wav_to_hdf5(*, audios_dir, csv_path, hdf5_path,
                 hf['waveform'][n] = float32_to_int16(audio)
                 hf['target'][n] = targets[n]
             else:
-                logging.info('{} File does not exist! {}'.format(n, audio_path))
+                logging.info(f'{n} - File does not exist: {audio_path}')
 
-    logging.info('Write to {}'.format(hdf5_path))
-    logging.info('Pack hdf5 time: {:.3f}'.format(time.time() - total_time))
+    fin_time = time.time()
+    logging.info(f'Written to {hdf5_path}')
+    logging.info(f'Pack time: {fin_time-start_time:.3f}')
  
 
-def create_indexes(*, hdf5_path, hdf5_index_path):
+def create_indexes(*, hdf5_path, hdf5_index_path, logs_dir=None):
     """Create indexes of hdf5-packed files for dataloader to read when training.
 
        :param str hdf5_path: Path of the hdf5-packed audios
        :param str hdf5_index_path: Path to save index of the packed audios
+       :param str logs_dir: Directory to save logs into, if None creates a
+                            directory 'logs' in CWD (default None)
        :return: None
        :rtype: None
     """
 
     # Paths
     create_folder(os.path.dirname(hdf5_index_path))
+    if logs_dir is None:
+        logs_dir = os.path.join(os.getcwd(), 'logs')
+    create_logging(logs_dir)
+
+    logging.info(f'Creating indexes for {hdf5_path}')
+    start_time = time.time()
 
     with h5py.File(hdf5_path, 'r') as hr:
         with h5py.File(hdf5_index_path, 'w') as hw:
@@ -103,15 +114,20 @@ def create_indexes(*, hdf5_path, hdf5_index_path):
             hw.create_dataset('hdf5_path', data=[hdf5_path.encode()] * audios_num, dtype='S200')
             hw.create_dataset('index_in_hdf5', data=np.arange(audios_num), dtype=np.int32)
 
-    print('Write to {}'.format(hdf5_index_path))
+    fin_time = time.time()
+    logging.info(f'Written to {hdf5_index_path}')
+    logging.info(f'Pack time: {fin_time-start_time:.3f}')
           
 
-def combine_indexes(*, hdf5_indexes_dir, hdf5_full_index_path, classes_num=110):
+def combine_indexes(*, hdf5_indexes_dir, hdf5_full_index_path, logs_dir=None,
+                       classes_num=110):
     """Combine several hdf5 indexes to a single hdf5.
 
        :param str indexes_hdf5_dir: Path to a directory with all indexes to be combined
        :param str full_indexes_hdf5_path: Path to save the combined index
        :param int classes_num: Amount of classes used in the dataset (default 110)
+       :param str logs_dir: Directory to save logs into, if None creates a
+                            directory 'logs' in CWD (default None)
        :return: None
        :rtype: None
     """
@@ -119,8 +135,14 @@ def combine_indexes(*, hdf5_indexes_dir, hdf5_full_index_path, classes_num=110):
     # Paths
     paths = get_sub_filepaths(hdf5_indexes_dir)
 
-    print('Total {} hdf5 to combine.'.format(len(paths)))
+    if logs_dir is None:
+        logs_dir = os.path.join(os.getcwd(), 'logs')
+    create_logging(logs_dir)
 
+    logging.info(f'Combining indexes from {hdf5_indexes_dir}')
+    logging.info(f'Total {len(paths)} hdf5 to combine.')
+
+    start_time_tot = time.time()
     with h5py.File(hdf5_full_index_path, 'w') as full_hf:
         full_hf.create_dataset(
             name='audio_name', 
@@ -148,7 +170,8 @@ def combine_indexes(*, hdf5_indexes_dir, hdf5_full_index_path, classes_num=110):
 
         for path in paths:
             with h5py.File(path, 'r') as part_hf:
-                print(path)
+                logging.info(f'Proicessing file {path}')
+                start_time = time.time()
                 n = len(full_hf['audio_name'][:])
                 new_n = n + len(part_hf['audio_name'][:])
 
@@ -163,8 +186,11 @@ def combine_indexes(*, hdf5_indexes_dir, hdf5_full_index_path, classes_num=110):
 
                 full_hf['index_in_hdf5'].resize((new_n,))
                 full_hf['index_in_hdf5'][n : new_n] = part_hf['index_in_hdf5'][:]
-                
-    print('Write combined full hdf5 to {}'.format(hdf5_full_index_path))
+                fin_time = time.time()
+                logging.info(f"Pack time {fin_time-start_time}')
+    fin_time_tot = time.time() 
+    logging.info(f'Written combined full hdf5 to {hdf5_full_index_path}')
+    logging.info(f'Total pack time: {fin_time_tot-start_time_tot:.3f}')
 
 
 if __name__ == '__main__':
@@ -176,6 +202,8 @@ if __name__ == '__main__':
                                        help='Path of packed waveforms hdf5.')
     parser_create_indexes.add_argument('--hdf5_index_path', type=str, required=True,
                                        help='Path to write out indexes hdf5.')
+    parser_create_indexes.add_argument('--logs_dir', type=str, default=None,
+                            help='Directory to save logs into.')
 
     parser_combine_full_indexes = subparsers.add_parser('combine_indexes')
     parser_combine_full_indexes.add_argument('--hdf5_indexes_dir', type=str,
@@ -184,6 +212,8 @@ if __name__ == '__main__':
             required=True, help='Path to write out full indexes hdf5 file.')
     parser_combine_full_indexes.add_argument('--classes_num', type=int, default=110,
                             help='The amount of classes used in the dataset.')
+    parser_combine_full_indexes.add_argument('--logs_dir', type=str, default=None,
+                            help='Directory to save logs into.')
 
     parser_waveforms_to_hdf5 = subparsers.add_parser('wav_to_hdf5')
     parser_waveforms_to_hdf5.add_argument('--audios_dir', type=str, required=True,
@@ -204,25 +234,29 @@ if __name__ == '__main__':
                             help='If specified, use only this many audios.')
     parser_waveforms_to_hdf5.add_argument('--clip_length', type=int, default=10000,
                 help='Length (in ms) of audio clips used in the dataset (default 10000)')
+    parser_waveforms_to_hdf5.add_argument('--logs_dir', type=str, default=None,
+                            help='Directory to save logs into.')
     args = parser.parse_args()
     
     if args.mode == 'create_indexes':
         create_indexes(hdf5_path=args.hdf5_path,
-                       hdf5_index_path=args.hdf5_index_path)
+                       hdf5_index_path=args.hdf5_index_path,
+                       logs_dir=args.logs_dir)
 
     elif args.mode == 'combine_indexes':
         combine_indexes(hdf5_indexes_dir=args.hdf5_indexes_dir,
                         hdf5_full_index_path=args.hdf5_full_index_path,
-                        classes_num=args.classes_num)
+                        classes_num=args.classes_num, logs_dir=args.logs_dir)
 
     elif args.mode == 'wav_to_hdf5':
-        waveforms_to_hdf5(audios_dir=args.audios_dir, csv_path=args.csv_path,
+        wav_to_hdf5(audios_dir=args.audios_dir, csv_path=args.csv_path,
                           class_list_path=args.class_list_path,
                           class_codes_path=args.class_codes_path,
                           clip_length=args.clip_length,
                           hdf5_path=args.hdf5_path,
                           sample_rate=args.sample_rate,
                           classes_num=args.classes_num,
-                          mini_data=args.mini_data)
+                          mini_data=args.mini_data,
+                          logs_dir=args.logs_dir)
     else:
         raise Exception('Incorrect arguments!')
