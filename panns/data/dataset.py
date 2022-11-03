f3c939b1405007f867a023e7c4e8b58e95b7e9ab
Alexander Bakhtin <alexander.bakhtin@tuni.fi>
2022-11-03 17:11:21 +0200

    add: rename panns.data.loaders.py to panns.data.dataset.py and change docstrings

--- panns/data/dataset.py
+++ panns/data/dataset.py
@@ -6,8 +6,19 @@ __all__ = ['AudioSetDataset']
 
 
 class AudioSetDataset(Dataset):
-    """Take meta of the audio clip, return waveform and target vector."""
+    """Class that returns an AudioSet segment audiofile and target from hdf5 file."""
     def __init__(self, hdf5_path, target_path):
+        """Initialize the dataset loading.
+
+           Parameters
+           __________
+           hdf5_path : str,
+               Path to hdf5 object containing a dataset 'waveform' of shape
+                                                    (audios_num, clip_length)
+           target_path : str,
+               Path to npy array storing the target of each audio clip
+               of shape (audios_num, ...)
+        """
         self._hdf5_data = h5py.File(hdf5_path, 'r')
         self._target = np.load(target_path)
 
@@ -19,9 +30,6 @@ class AudioSetDataset(Dataset):
         return self._target.shape[0]
 
     def __getitem__(self, i):
-        """Load waveform and target of an audio clip.
-        """
-
         waveform = self._hdf5_data['waveform'][i]
         target = self._target[i, :]
 
