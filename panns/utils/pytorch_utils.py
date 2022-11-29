4144ae7c537cb5d2f7db2aa3d079a38b8b2c8a47
Alexander Bakhtin <alexander.bakhtin@tuni.fi>
2022-11-29 16:06:58 +0200

    fix: improve formatting, fix namespace errors

--- panns/utils/pytorch_utils.py
+++ panns/utils/pytorch_utils.py
@@ -35,7 +35,9 @@ def pad_framewise_output(framewise_output, frames_num):
     Outputs:
       output: (batch_size, frames_num, classes_num)
     """
-    pad = framewise_output[:, -1 :, :].repeat(1, frames_num - framewise_output.shape[1], 1)
+    pad = framewise_output[:, -1:, :].repeat(1,
+                                             frames_num -
+                                             framewise_output.shape[1], 1)
     """tensor for padding"""
 
     output = torch.cat((framewise_output, pad), dim=1)
@@ -52,85 +54,96 @@ def count_flops(model, audio_length):
     """Count flops. Code modified from others' implementation.
     """
     multiply_adds = True
-    list_conv2d=[]
-    def conv2d_hook(self, input, output):
-        batch_size, input_channels, input_height, input_width = input[0].size()
+    list_conv2d = []
+
+    def conv2d_hook(self, data, output):
+        batch_size, input_channels, input_height, input_width = data[0].size()
         output_channels, output_height, output_width = output[0].size()
- 
-        kernel_ops = self.kernel_size[0] * self.kernel_size[1] * (self.in_channels / self.groups) * (2 if multiply_adds else 1)
+
+        kernel_ops = self.kernel_size[0] * self.kernel_size[1] * (
+                self.in_channels / self.groups) * (
+                         2 if multiply_adds else 1)
         bias_ops = 1 if self.bias is not None else 0
- 
+
         params = output_channels * (kernel_ops + bias_ops)
         flops = batch_size * params * output_height * output_width
- 
+
         list_conv2d.append(flops)
 
-    list_conv1d=[]
-    def conv1d_hook(self, input, output):
-        batch_size, input_channels, input_length = input[0].size()
+    list_conv1d = []
+
+    def conv1d_hook(self, data, output):
+        batch_size, input_channels, input_length = data[0].size()
         output_channels, output_length = output[0].size()
- 
-        kernel_ops = self.kernel_size[0] * (self.in_channels / self.groups) * (2 if multiply_adds else 1)
+
+        kernel_ops = self.kernel_size[0] * (self.in_channels / self.groups) * (
+            2 if multiply_adds else 1)
         bias_ops = 1 if self.bias is not None else 0
- 
+
         params = output_channels * (kernel_ops + bias_ops)
         flops = batch_size * params * output_length
- 
+
         list_conv1d.append(flops)
- 
-    list_linear=[] 
-    def linear_hook(self, input, output):
-        batch_size = input[0].size(0) if input[0].dim() == 2 else 1
- 
+
+    list_linear = []
+
+    def linear_hook(self, data, output):
+        batch_size = data[0].size(0) if data[0].dim() == 2 else 1
+
         weight_ops = self.weight.nelement() * (2 if multiply_adds else 1)
         bias_ops = self.bias.nelement()
- 
+
         flops = batch_size * (weight_ops + bias_ops)
         list_linear.append(flops)
- 
-    list_bn=[] 
-    def bn_hook(self, input, output):
-        list_bn.append(input[0].nelement() * 2)
- 
-    list_relu=[] 
-    def relu_hook(self, input, output):
-        list_relu.append(input[0].nelement() * 2)
- 
-    list_pooling2d=[]
-    def pooling2d_hook(self, input, output):
-        batch_size, input_channels, input_height, input_width = input[0].size()
+
+    list_bn = []
+
+    def bn_hook(self, data, output):
+        list_bn.append(data[0].nelement() * 2)
+
+    list_relu = []
+
+    def relu_hook(self, data, output):
+        list_relu.append(data[0].nelement() * 2)
+
+    list_pooling2d = []
+
+    def pooling2d_hook(self, data, output):
+        batch_size, input_channels, input_height, input_width = data[0].size()
         output_channels, output_height, output_width = output[0].size()
- 
+
         kernel_ops = self.kernel_size * self.kernel_size
         bias_ops = 0
         params = output_channels * (kernel_ops + bias_ops)
         flops = batch_size * params * output_height * output_width
- 
+
         list_pooling2d.append(flops)
 
-    list_pooling1d=[]
-    def pooling1d_hook(self, input, output):
-        batch_size, input_channels, input_length = input[0].size()
+    list_pooling1d = []
+
+    def pooling1d_hook(self, data, output):
+        batch_size, input_channels, input_length = data[0].size()
         output_channels, output_length = output[0].size()
- 
+
         kernel_ops = self.kernel_size[0]
         bias_ops = 0
-        
+
         params = output_channels * (kernel_ops + bias_ops)
         flops = batch_size * params * output_length
- 
+
         list_pooling2d.append(flops)
- 
+
     def foo(net):
-        childrens = list(net.children())
-        if not childrens:
+        children = list(net.children())
+        if not children:
             if isinstance(net, nn.Conv2d):
                 net.register_forward_hook(conv2d_hook)
             elif isinstance(net, nn.Conv1d):
                 net.register_forward_hook(conv1d_hook)
             elif isinstance(net, nn.Linear):
                 net.register_forward_hook(linear_hook)
-            elif isinstance(net, nn.BatchNorm2d) or isinstance(net, nn.BatchNorm1d):
+            elif isinstance(net, nn.BatchNorm2d) or isinstance(net,
+                                                               nn.BatchNorm1d):
                 net.register_forward_hook(bn_hook)
             elif isinstance(net, nn.ReLU):
                 net.register_forward_hook(relu_hook)
@@ -141,18 +154,19 @@ def count_flops(model, audio_length):
             else:
                 print('Warning: flop of module {} is not counted!'.format(net))
             return
-        for c in childrens:
+        for c in children:
             foo(c)
 
     # Register hook
     foo(model)
-    
-    device = device = next(model.parameters()).device
-    input = torch.rand(1, audio_length).to(device)
 
-    out = model(input)
- 
+    device = next(model.parameters()).device
+    data = torch.rand(1, audio_length).to(device)
+
+    model(data)
+
     total_flops = sum(list_conv2d) + sum(list_conv1d) + sum(list_linear) + \
-        sum(list_bn) + sum(list_relu) + sum(list_pooling2d) + sum(list_pooling1d)
-    
+                  sum(list_bn) + sum(list_relu) + sum(list_pooling2d) + sum(
+            list_pooling1d)
+
     return total_flops
