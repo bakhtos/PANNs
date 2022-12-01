1138a034d671d1b92241c2f89384771fbdd5a97e
Alexander Bakhtin <alexander.bakhtin@tuni.fi>
2022-12-01 06:30:06 +0200

    add: change docstring

--- panns/evaluate.py
+++ panns/evaluate.py
@@ -6,21 +6,14 @@ from .forward import forward
 def evaluate(model, data_loader):
     """Forward evaluation data to the model and calculate AP and AUC metrics.
 
-    Parameters
-    __________
+    Args:
+        model: torch.nn.Module subclass, Torch model to be evaluated
+        data_loader: torch.utils.data.Dataloader, Data loader for the evaluation data
 
-    model : torch.nn.Module subclass,
-        Torch model to be evaluated
-    data_loader: torch.utils.data.Dataloader,
-        Data loader for the evaluation data
-
-    Returns
-    _______
-
-    average_precision : float,
-        Average precision score metric result from sklearn
-    auc: float,
-        Area under curve metric result from sklearn
+    Returns:
+    average_precision, auc
+        -Average precision score metric result from sklearn
+        -Area under curve metric result from sklearn
     """
 
     # Forward
