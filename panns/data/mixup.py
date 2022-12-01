86931093861b4dd79830bb9179a25d4580e7a34e
Alexander Bakhtin <alexander.bakhtin@tuni.fi>
2022-12-01 06:21:06 +0200

    add: change docstrings, add annotations

--- panns/data/mixup.py
+++ panns/data/mixup.py
@@ -4,23 +4,22 @@ __all__ = ['mixup_coefficients',
            'mixup']
 
 
-def mixup_coefficients(mixup_alpha, batch_size=32, random_seed=1234):
+def mixup_coefficients(mixup_alpha: float, batch_size: int = 32,
+                       random_seed: int = 1234) -> np.ndarray:
     """Sample lambda coefficients for mixup from beta distribution.
-    Parameters
-    __________
-    mixup_alpha : float,
-        Will be used as both parameters for the beta distribution
-    batch_size : int, optional (default 32)
-        Batch size to generate lambdas for (must be twice as big as the
-            actual batch size passed to model)
-    random_seed : int, optional (default 1234)
-        Seed to give to numpy.random.default_rng
 
-    Yields
-    ______
-    lambdas : numpy.ndarray,
-        Each call fields a batch of lambda coefficients
+    Args:
+        mixup_alpha: float, Will be used as both parameters for the beta distribution
+        batch_size: int, Batch size to generate lambdas for;
+            must be twice as big as the actual batch size passed to model
+            (default 32)
+        random_seed: int, Seed to give to numpy.random.default_rng
+            (default 1234)
+
+    Returns: numpy.ndarray, Each call yields a batch of lambda coefficients
+
     """
+
     random_state = np.random.default_rng(random_seed)
     while True:
         lambdas = np.zeros(batch_size)
@@ -33,17 +32,13 @@ def mixup_coefficients(mixup_alpha, batch_size=32, random_seed=1234):
 def mixup(x, mixup_lambda):
     """Mixup x of even indexes (0, 2, 4, ...) with x of odd indexes (1, 3, 5, ...).
 
-    Parameters
-    __________
-    x : numpy.ndarray,
-        Batch of data of shape (batch_size * 2, ...) to be mixed
-    mixup_lambda : numpy.ndarray,
-        Array of shape (batch_size * 2,), lambda coefficients for mixup
+    Args:
+        x: numpy.ndarray, Batch of data of shape (batch_size * 2, ...) to be mixed
+        mixup_lambda: numpy.ndarray, Array of shape (batch_size * 2,),
+            lambda coefficients for mixup
 
-    Returns
-    _______
-      :return out: Array of shape (batch_size, ...) with performed mixup
-      :rtype: numpy.ndarray
+    Returns: numpy.ndarray, Array of shape (batch_size, ...) with performed mixup
+
     """
 
     out = x.transpose(-1, 0) * mixup_lambda
