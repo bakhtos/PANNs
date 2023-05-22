38127d49dd5c5e1f60da25150edbec690400e3cf
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2023-05-22 15:02:31 +0300

    fix: typo in 'levelname'

--- panns/base_logging.py
+++ panns/base_logging.py
@@ -4,7 +4,7 @@ logger = logging.getLogger("panns")
 logger.setLevel(logging.DEBUG)
 stream = logging.StreamHandler()
 stream.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s \t %(levelnames)s \t %(name)s \t %(message)s')
+formatter = logging.Formatter('%(asctime)s \t %(levelname)s \t %(name)s \t %(message)s')
 stream.setFormatter(formatter)
 logger.addHandler(stream)
 
